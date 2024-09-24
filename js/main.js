3169f84c71a62efeddf4676104d27f1b20e0138e
Joan Costa <joan.costa.mombiela@gmail.com>
2024-09-24 08:32:21 +0200

    Versión inicial

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Cookies
+$(document).ready(function(){
+	checkCookies();
+	$("#btn_accept_cookies").click(accept);
+	mathReload();
+});
+    
+function checkCookies()
+{
+	var x = Cookies.get('cookies_informed');
+	if (x == "OK") $("#cookies_adv").remove();
+	else $("#cookies_adv").show();
+}
+
+function accept()
+{
+	Cookies.set('cookies_informed', "OK", {expires: 365});
+	$("#cookies_adv").remove();
+}
